99c3a1a366b1a51111a269bb4fc01d9db5d26ef6
AWhiteKnight <awhiteknight@unity-mail.de>
2019-04-05 11:41:40 +0200

    try to understand testing

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,16 @@
 const path = require("path");
 const { tests } = require("@iobroker/testing");
 
+// You can also mock external modules to create a more controlled environment during testing.
+// Define the mocks as objects and include them below
+const nobleMock = {
+    on() {},
+    state: "poweredOff",
+}
+
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+tests.unit(path.join(__dirname, ".."), {
+    //     ~~~~~~~~~~~~~~~~~~~~~~~~~
+    // This should be the adapter's root directory
+
+});
