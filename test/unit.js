1d36ee58f2677d682de60f86ac78fe9c492826e9
AWhiteKnight <awhiteknight@unity-mail.de>
2019-05-22 12:30:42 +0200

    testing stuff

--- test/unit.js
+++ test/unit.js
@@ -13,6 +13,22 @@ tests.unit(path.join(__dirname, ".."), {
     //     ~~~~~~~~~~~~~~~~~~~~~~~~~
     // This should be the adapter's root directory
 
+   // If the adapter may call process.exit during startup, define here which exit codes are allowed.
+    // By default, no exit codes are allowed.
+    // allowedExitCodes: [11],
+
+    // If the adapter startup unit tests sometimes fail with a timeout, 
+    // you can optionally increase the default timeout here.
+    // startTimeout: 60000, // 15000 is the default
+
+    // optionally define which modules should be mocked.
+    additionalMockedModules: {
+        "noble": nobleMock,
+        "@abandonware/noble": nobleMock,
+        // Use the {CONTROLLER_DIR} placeholder to access the path where JS-Controller would be installed.
+        // Don't forget to provide mocks for every module you need, as they don't exist in unit tests
+        "{CONTROLLER_DIR}/lib/tools.js": {}, 
+    },
 
     // Optionally overwrite the default adapter config
     // overwriteAdapterConfig(config: Record<string, any>) {
@@ -59,6 +75,23 @@ tests.unit(path.join(__dirname, ".."), {
         config.versions = true;
         // Don't forget to return it
         return config;
-    }
-    
+    },
+
+    // If the startup tests need require specific behavior of the mocks 
+    // or if you are using methods that don't have a default implementation on the mocks,
+    // you can define the behavior here. This method is called before every predefined test
+    defineMockBehavior(database, adapter) {
+        // e.g.
+        // adapter.objects.rename.callsFake( /* implementation here */ );
+        // or
+        // adapter.objects.getUserGroup.returns("a string");
+    },
+
+    // Define your own tests inside defineAdditionalTests. 
+    // If you need predefined objects etc. here, you need to take care of it yourself
+    defineAdditionalTests() {
+        // it("works", () => {
+            // see below how these could look like
+        // });
+    },
 });
